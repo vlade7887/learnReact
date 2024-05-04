c98469100490bc44e42c44072196eeb94049b34c
enriquezvernie <enriquezvernie@gmail.com>
2024-05-04 23:13:46 +0800

    add ternary

---
+++ ternary/src/components/App.js
@@ -0,0 +1,28 @@
+import Login from "./Login";
+
+var isLoggedIn = true;
+
+function renderConditionally(){
+  if (!isLoggedIn){
+    return (
+      <h1>Hello</h1>
+    )
+  }
+  else{
+    return(
+      <Login />
+    )
+  }
+}
+
+
+function App(){
+  return(
+    <div className="container">
+     {renderConditionally()}
+       
+    </div>
+  )
+}
+
+export default App;
---
+++ ternary/src/components/Input.js
@@ -0,0 +1,12 @@
+function Input(props){
+  console.log(props)
+  return (
+    <div>
+    <input type={props.type} placeholder={props.placeholder} />
+  
+    </div>
+  )
+}
+
+
+export default Input;
